644575ec0aa5c715b6add99c4f0528c20c875aa0
Kentaro Kato <ken.k.jul2@gmail.com>
2023-06-22 07:01:26 +0900

    NavBarの下線部アニメーション

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import React from "react";
+import style from "./Page.module.css";
 
 const menu_items = [
   {
@@ -14,14 +15,21 @@ const menu_items = [
 
 const PlayGround = () => {
   return (
-    <nav className="my-5 mx-20 flex justify-between">
-      <h2 className="text-lg font-semibold">PageTitle</h2>
+    <nav className="my-5 mx-60 flex justify-between">
+      <h2 className="text-lg font-semibold gap">PageTitle</h2>
       {/* <div className="flex gap-10 text-red-300">
         {menu_items.map((item) => item.title)}
       </div> */}
-      <div className="flex gap-3">
-        <Link href="/">Home</Link>
-        <Link href="/">About</Link>
+      <div className="flex-row flex gap-4">
+        <Link className={style.nav_items} href="/">
+          <span className={style.item}>Home</span>
+        </Link>
+        <Link className={style.nav_items} href="/">
+          <span className={style.item}>About</span>
+        </Link>
+        <Link className={style.nav_items} href="/">
+          <span className={style.item}>Contact</span>
+        </Link>
       </div>
     </nav>
   );
