0a7a352a652d03c16df5bab36a503a6875b7db51
Kentaro Kato <ken.k.jul2@gmail.com>
2023-07-13 06:39:01 +0900

    complete/Components分割

---
+++ src/components/ThisIsSection.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ThisIsSection = () => {
+  return (
+    <section>
+      <div className="container bg-slate-800 px-4 py-20 mx-auto">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="text-5xl mb-5">This is ...</h1>
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quod
+            consequatur mollitia omnis accusamus architecto vitae ducimus
+            provident aspernatur ea!
+          </h3>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ThisIsSection;
